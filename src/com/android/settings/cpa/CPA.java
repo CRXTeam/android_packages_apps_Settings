0385b5d35e6407f7cf686c9f513fb24647146a3e
zodex <mimirower45@wp.pl>
2015-01-29 17:32:59 +0100

    Forward port: CPA Settings

---
+++ src/com/android/settings/cpa/CPA.java
@@ -0,0 +1,92 @@
+/*
+* Copyright (C) 2014 -2015 The CPA2 Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.settings.cpa;
+
+import java.io.IOException;
+
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.app.AlertDialog;
+import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.RemoteException;
+import android.os.ServiceManager;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.os.Handler;
+import android.util.Log;
+import android.net.Uri;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.view.IWindowManager;
+import android.os.ServiceManager;
+import android.os.IBinder;
+import android.os.IPowerManager;
+import android.view.WindowManagerGlobal;
+
+import android.provider.Settings;
+import android.os.SystemProperties;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CPA extends SettingsPreferenceFragment implements Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "CPA";
+    private static final boolean DEBUG = true;
+
+    private final Configuration mCurrentConfig = new Configuration();
+
+    private Context mContext;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.cpa_settings);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+}
+
+
